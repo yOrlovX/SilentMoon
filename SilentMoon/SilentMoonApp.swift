5611c675bbe11be21527de5b7e292ae706d31bee
Yaroslav Orlov <ulverforest@gmail.com>
2023-05-24 16:14:59 +0300

    Initial Commit

---
+++ SilentMoon/SilentMoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SilentMoonApp.swift
+//  SilentMoon
+//
+//  Created by Yaroslav Orlov on 24.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct SilentMoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
